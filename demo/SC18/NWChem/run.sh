413909163a31d245e371a8765a5f810dc8e5c896
Gyorgy Matyasfalvi <gmatyasfalvi@bnl.gov>
2018-11-08 19:11:36 -0500

    Updated run.sh to fit new Dockerimage.

--- demo/SC18/NWChem/run.sh
+++ demo/SC18/NWChem/run.sh
@@ -2,11 +2,15 @@
 
 set -m
 
+. /etc/profile
+
+export PYTHONPATH=/Chimbuko/PerformanceAnalysis/lib/codar/chimbuko/perf_anom:/Install/adios-1.13.1/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
 cd Visualization/
 
 python3 main.py &
 
-cd ../Analysis/drivers
+cd ../PerformanceAnalysis/drivers
 
 bash run_chimbuko.sh chimbuko_tau-nwchem.cfg && bg
 
