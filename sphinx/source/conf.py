948467e8df4943a0347d8caed743d86213857c2b
Sungsoo Ha <sungsooha@visws.csi.bnl.gov>
2019-11-15 17:28:08 -0500

    init document structure

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -47,7 +47,9 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = [
+    
+]
 
 pygments_style = 'sphinx'
 
